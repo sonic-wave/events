48d23c34d7d2ed93ae3b5ad7ab7d416198009e28
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-12-19 13:37:35 +0300

    webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-const OptimizeCSSAssetsPlugin = require('css-minimizer-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    minimize: true,
     minimizer: [
-      new OptimizeCSSAssetsPlugin({}),
-      '...',
+      new CssMinimizerPlugin(),
     ],
   },
 });
